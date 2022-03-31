4fe2a530475965650534c7e1b86def91b3dccd3e
rhyrichard <richard.adam112@gmail.com>
2022-03-31 15:24:44 +0700

    Membuat sebuah Template sederhana dan dinamis Untuk Tugas pertemuan 3 File yang dibuat yaitu v_index,v_header,v_footer,v_about.php dan template warna di folder assets/css/stylebuku.css. Kemudian setting base_url() pada codeigniter

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Richard</li>
+ <li>Nama Belakang : Adam</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan Intan 1 Nomor 664</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Jakarta</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>E-Sport</li>
+ <li>Mental</li>
+ </ul>
+</section>
