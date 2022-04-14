46c4d0ae9f0966113a4f84ab56c2b14e4d6e9262
rhyrichard <richard.adam112@gmail.com>
2022-04-14 14:03:29 +0700

    Matakuliah,form matakuliah

--- application/views/view-form-matakuliah.php
+++ application/views/view-form-matakuliah.php
@@ -4,13 +4,14 @@
 </head>
 
 <body>
+
  <center>
     <form action="<?= base_url('matakuliah/cetak'); ?>"
         method="post">
 
- <table>
- <tr>
- <th colspan="3">
+    <table>
+    <tr>
+        <th colspan="3">
  Form Input Data Mata Kuliah
  </th>
  </tr>
